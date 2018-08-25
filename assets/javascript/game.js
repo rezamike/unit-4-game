d11c813736972a8ecb8c6db72ab36c78aa376a2c
rezamike <michael.sanaiha@gmail.com>
2018-08-25 12:56:16 -0700

    variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+$(function () {
+
+// -------static variables
+let score = "";
+let randomNum = (Math.floor(Math.random() * 102) + 19);
+let win = 0;
+let loss = 0;
+let winLoss = [
+    {win: "Wow. You won. Do you want a cookie or something?"},
+    {loss: "Damn, you suck..."}
+]
+
+console.log(randomNum);
+
+// --------app state
+
+
+// cached DOM elements
+
+
+// event listeners
+
+
+// functions
+
+// checking winner
+
+
+// initalize
+
+
+
+})
