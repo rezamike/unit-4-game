5da2339704d7a22ce8721cd6c463dec7f0a0dd38
rezamike <michael.sanaiha@gmail.com>
2018-08-25 15:25:56 -0700

    i think i have a good wireframe now and building html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,62 @@
 $(function () {
 
-// -------static variables
-let score = "";
-let randomNum = (Math.floor(Math.random() * 102) + 19);
-let win = 0;
-let loss = 0;
-let winLoss = [
-    {win: "Wow. You won. Do you want a cookie or something?"},
-    {loss: "Damn, you suck..."}
-]
+    // -------static variables
+    let score = 0;
+    let randomNum = (Math.floor(Math.random() * 102) + 19);
+    let win = 0;
+    let loss = 0;
+    let winLoss = {
+        win: "Wow. You won. Do you want a cookie or something?",
+        loss: "Damn, you suck..."
+    };
+    let crystal = {
+        one: (Math.floor(Math.random() * 12) + 1),
+        two: (Math.floor(Math.random() * 12) + 1),
+        three: (Math.floor(Math.random() * 12) + 1),
+        four: (Math.floor(Math.random() * 12) + 1)
+    };
 
-console.log(randomNum);
-
-// --------app state
-
-
-// cached DOM elements
-
-
-// event listeners
+    // --------app state
 
 
-// functions
+    // cached DOM elements
+    console.log(randomNum);
 
-// checking winner
+
+    // event listeners --- connecting buttons to random number
+    $(".button").on("click", function game() {
+
+        if ($(this).hasClass("btn1")) {
+            score += crystal.one;
+            console.log(crystal.one);
+        }
+        else if ($(this).hasClass("btn2")) {
+            score += crystal.two;
+            console.log(crystal.two);
+        }
+        else if ($(this).hasClass("btn3")) {
+            score += crystal.three;
+            console.log(crystal.three);
+        }
+        else if ($(this).hasClass("btn4")) {
+            score += crystal.four;
+            console.log(crystal.four);
+        }
+        console.log(score);
+        if (score === randomNum) {
+            win++;
+        }
+    });
 
 
-// initalize
+    // functions
+    
+
+
+    // checking winner
+    
+
+    // initalize
 
 
 
